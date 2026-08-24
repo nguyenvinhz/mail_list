dd03aed6ab509f12a030bb266f607cf2e88381b7
Vinh Nguyen <nguyenvinh0712@gmail.com>
2026-08-24 11:25:27 +0700

    firt commit

---
+++ src/main/java/murach/business/User.java
@@ -0,0 +1,56 @@
+package murach.business;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String dob;
+
+    public User() {
+        firstName = "";
+        lastName = "";
+        email = "";
+        dob = "";
+    }
+
+    public User(String firstName, String lastName, String email, String dob) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.dob = dob;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+}
